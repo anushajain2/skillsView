4e1aaffa18bbb3b175c4832be152b68f22ea5008
AnuTess <aj274@cornell.edu>
2018-07-07 20:52:05 -0400

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '100839412134-l3mnqu3ahmvhen3g3r3vn9tum8fr89u9.apps.googleusercontent.com',
+    googleClientSecret: 'DcMKFG65uciZbz9OA-E7Q2u1',
+    mongoURI: 'mongodb://skillsanu:lalaKORI12@ds211029.mlab.com:11029/chopsview-dev',
+    cookieKey: '346587vf26bg2t6ry3847r6cfg2c9634rfyn92843r9834ychun4t03t'
+}
